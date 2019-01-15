94ade89f49749b24e2d518b9893c08f5786f084d
Kenford <kzhou0066@gmail.com>
2019-01-14 21:41:21 -0500

    cleaned up some styles

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     return ( 
         <div>
             <div id="contact-header">
-                <span><i class="fa fa-envelope"></i></span>
+                <span><i class="fas fa-comment"></i></span>
                 <h2 className="section-headers"> Get in touch </h2>
                 <h3> Use the form below to begin a conversation </h3>
             </div>
@@ -39,28 +39,28 @@ const Contact = () => {
                 <div id="contact-info-wrapper">
                     <h3> Contact Information </h3>
                     <hr />
-                    <span><i class="fas fa-code"></i></span>
+                    <i class="fas fa-user-alt"></i>
                     <h5> Kenny Zhou </h5>
                     <br />
-                    <span><i class="fas fa-code"></i></span>
+                    <i class="fas fa-city city-icon"></i>
                     <h5> Chicago, Illinois </h5>
                     <br />
-                    <span><i class="fas fa-code"></i></span>
+                    <i class="fas fa-globe-americas"></i>
                     <h5> United States </h5>
                     <br />
-                    <span><i class="fas fa-code"></i></span>
+                    <i class="fas fa-phone-volume phone-icon"></i>
                     <h5> 312-532-0066 </h5>
                     <br />
-                    <span><i class="fas fa-code"></i></span>
+                    <i class="fas fa-envelope-open"></i>
                     <h5> kzhou1013@gmail.com </h5>
                     <br />
                     <hr />
 
                     <p id="alt-contact-header">...or contact me via: </p>
                     <div id="alt-contact-wrapper">
-                        <span><i class="fas fa-code"></i></span>
-                        <span><i class="fas fa-code"></i></span>
-                        <span><i class="fas fa-code"></i></span>
+                        <i class="fas fa-code"></i>
+                        <i class="fas fa-code"></i>
+                        <i class="fas fa-code"></i>
                     </div>
                 </div>
             </div>
