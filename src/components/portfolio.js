368d497a54240baf794296b589749b35a32ccbb8
Kenford <kzhou0066@gmail.com>
2019-01-23 01:27:23 -0500

    switched out media queries for a mixin

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -17,9 +17,9 @@ const Portfolio = () => {
                     <hr />
                     <p className="portfolio-item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     <span className="portfolio-item-tech"> MongoDB </span>
+                    <span className="portfolio-item-tech"> Node </span>
                     <span className="portfolio-item-tech"> Express </span>
                     <span className="portfolio-item-tech"> React </span>
-                    <span className="portfolio-item-tech"> Node </span>
                 </div>
                 <Thumbnail 
                     altTag = "Cajewelers Thumbnail"
